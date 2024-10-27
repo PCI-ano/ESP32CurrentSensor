b5545c3e3d36455488ab9be9fee07c67f997317e
PCI-ano <150927769+PCI-ano@users.noreply.github.com>
2024-10-27 09:25:55 +0900

    関数を追加

---
+++ main.py
@@ -0,0 +1,17 @@
+from machine import Timer, ADC, I2C
+import time
+import math
+from display.aqm1602a import AQM1602A
+
+
+def main():
+    adc32 = ADC(32, atten=ADC.ATTN_0DB)
+
+
+
+def measureRMSCurrent(adc, ratio, resistance):
+    sum = 0
+
+
+if __name__ == "__main__":
+    main()
